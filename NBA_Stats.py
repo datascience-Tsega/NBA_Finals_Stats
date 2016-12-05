7f42e2474af57e0bd16cf5b6c42d26d37ccd1f16
datascienceTsega <tsmengistu@gmail.com>
2016-12-04 19:19:07 -0600

    Added Comments

--- NBA_Stats.py
+++ NBA_Stats.py
@@ -14,11 +14,12 @@ for col in df.columns:
 del df['Lg']
 
 df2 = df[df.Series =='Finals'] #get only finals games
-
 df3 = df2[['Yr','Team','W','Team.1','W.1']] #get only necessary columns
 df3['Total_Games'] = df3['W'] + df3['W.1'] #create a new column showing total games played in each series
 df3.index = range (0,76,1) #reset index
 
-for i in range(4,8):
+#results
+for i in range(4,8): #iterate through the 4 possibilities (4-game, 5-game, 6-game and 7-game series)
+    #count each outcome and divide it by the total number of nba finals series, 76 (years 1950 to 2016)
     no_of_games = float(df3[df3.Total_Games ==i]['Total_Games'].size)/76.
     print "NBA finals series went to %i-games, "%(i)+'{percent:.1%}'.format(percent = no_of_games)+" of the time"
